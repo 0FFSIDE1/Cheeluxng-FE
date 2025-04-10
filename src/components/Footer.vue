557d250d4c78dff91dc12c78f2cfe0fd33c02c3c
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-04-10 17:03:24 +0100

    feat: update Footer component layout and content for better user engagement

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,24 @@
 <template>
-    <div class="w-full h-32 bg-black flex justify-center items-center text-white">
-        <p>This is a footer</p>
+    <div class="flex text-white bg-black w-full min-h-64 justify-between items-center p-4 pl-12">
+        <div>
+            <h2>Stay Connected</h2>
+            <ul>
+                <li>2025 © Cheeluxng Abuja</li>
+                <li>Helpline: +234 708 470 4784</li>
+                <li>Opening Hours: Tuesday - Saturday 8:00 am - 9:00 pm</li>
+            </ul>
+        </div>
+        <div> <img src="../assets/whitelogo.png" class="object-fit w-full h-64" alt="logo"></div>
+       
+        <div class="mr-12">
+            <h2>Helpful Links</h2>
+            <ul>
+                <a href="http://"><li>About Us</li></a>
+                <a href="http://"><li>Our trading policy</li></a>
+                <a href="http://"><li>Trade instructions</li></a>
+                <a href="http://"><li>Search</li></a>
+            </ul>
+
+        </div>
     </div>
 </template>
