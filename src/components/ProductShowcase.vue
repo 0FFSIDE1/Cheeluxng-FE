0e6d625ac6895a1ba77f19c032c3bc457ac40b73
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-04-10 22:39:12 +0100

    feat: add ProductShowcase component for displaying product details

---
+++ src/components/ProductShowcase.vue
@@ -0,0 +1,65 @@
+<template>
+    <section class="my-14 mx-1 h-full">
+      <div class="flex justify-center items-center flex-col gap-10 relative">
+        <h2 class="text-gray-900">{{ title }}</h2>
+        <!-- Main image container -->
+        <div class="relative">
+          <img
+            :src="mainProduct.image"
+            :alt="mainProduct.name"
+            class="object-cover rounded-lg h-[750px] w-[700px]"
+          />
+  
+          <div
+            v-if="secondaryProduct"
+            class="hidden md:block absolute bottom-0 right-[-110px] h-[450px] w-[300px] bg-slate-200 rounded-lg p-4 shadow-md"
+          >
+            <img
+              :src="secondaryProduct.image"
+              :alt="secondaryProduct.name"
+              class="object-cover rounded-lg"
+            />
+            <div>
+              <h3 class="text-gray-900">{{ secondaryProduct.name }}</h3>
+              <p class="text-gray-900">{{ secondaryProduct.price }}</p>
+              <div class="flex justify-between items-center">
+                <ul class="flex gap-4">
+                  <li
+                    v-for="(color, index) in secondaryProduct.colors"
+                    :key="index"
+                    :class="`w-4 h-4 rounded-full ${color}`"
+                  ></li>
+                </ul>
+                <button class="bg-gray-900 w-12 rounded-md p-1 text-white">
+                  Buy
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      title: {
+        type: String,
+        required: true,
+      },
+      mainProduct: {
+        type: Object,
+        required: true,
+      },
+      secondaryProduct: {
+        type: Object,
+        required: false, // Optional secondary product
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Add any specific styles here if needed */
+  </style>
