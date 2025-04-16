a86459ee5e1a034fd5544706ec3dd523a879255c
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-04-16 13:37:23 +0100

    chore: comment out custom screen breakpoints in tailwind.config.js for clarity

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ export default {
   ],
   theme: {
     extend: {},
+    // screens: {
+    //   'xs': '400px',         // custom breakpoint
+    //   'sm': '896px',
+    //   'md': '768px',
+    //   'lg': '1024px',
+    //   'xl': '1280px',
+    //   '2xl': '1536px',
+    //   '3xl': '1920px'        // another custom one
+    // },
   },
   plugins: [],
 }
