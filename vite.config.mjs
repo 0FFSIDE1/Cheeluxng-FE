1cf4a5429be3d95291332425b4239b18673d36bc
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-04-16 13:37:33 +0100

    chore: update vite.config.mjs to include allowedHosts and host settings for server configuration

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,6 +4,8 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: 3000
+    port: 3000,
+    allowedHosts: ['bca3-105-113-65-85.ngrok-free.app'],
+    host: true,
   }
 })
