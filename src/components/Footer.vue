9522554f778e2f45526fcf559db8ae042acfa2f7
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-04-16 20:08:21 +0100

    fix: improve layout and structure of Footer component for better readability

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,24 +1,40 @@
 <template>
-    <div class="flex text-white bg-black min-h-64 justify-between items-center p-4 pl-12">
-        <div>
+    
+    <div class="flex flex-col md:flex-col text-white bg-black min-h-64 justify-between items-center p-4">
+        <div> <img src="../assets/whitelogo.png" class="object-fit w-full h-72" alt="logo"></div>
+
+        <div class=" bg-black min-h-64 flex flex-col gap-12 md:flex-row justify-between lg:gap-96  text-white">
+        <div class="flex flex-col gap-4 items-center">
             <h2>Stay Connected</h2>
             <ul>
-                <li>2025 © Cheeluxng Abuja</li>
                 <li>Helpline: +234 708 470 4784</li>
                 <li>Opening Hours: Tuesday - Saturday 8:00 am - 9:00 pm</li>
             </ul>
         </div>
-        <div> <img src="../assets/whitelogo.png" class="object-fit w-full h-64" alt="logo"></div>
+        
        
-        <div class="mr-12">
+        <div class=" w-72 flex flex-col gap-4 items-center">
+            <h2>Follow Us</h2>
+            <ul class="flex flex-row gap-4">
+                <a href="http://"><li><img src="../assets/instagram.png" alt=""></li></a>
+                <a href="http://"><li><img src="../assets/facebook.png" alt=""></li></a>
+                <a href="http://"><li><img src="../assets/twitter.png" alt=""></li></a>
+                <a href="http://"><li><img src="../assets/tiktok.png" alt=""></li></a>
+                <a href="http://"><li><img src="../assets/youtube.png" alt=""></li></a>
+                <a href="http://"><li><img src="../assets/whatsapp.png" alt=""></li></a>
+            </ul>
             <h2>Helpful Links</h2>
-            <ul>
+            <ul class="flex flex-col gap-4 justify-center items-center">
                 <a href="http://"><li>About Us</li></a>
                 <a href="http://"><li>Our trading policy</li></a>
                 <a href="http://"><li>Trade instructions</li></a>
-                <a href="http://"><li>Search</li></a>
+                
             </ul>
 
         </div>
     </div>
+        <p class="mt-10">2025 © Cheeluxng Abuja</p>
+    </div>
+    
+    
 </template>
