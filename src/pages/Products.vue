131fbb0f0a5e96aa237c435fec0a2ebd7e6dae5f
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-04-17 18:39:17 +0100

    feat: add Products.vue page with initial template and content

---
+++ src/pages/Products.vue
@@ -0,0 +1,6 @@
+<template>
+    <section class="text-center py-10">
+      <h2 class="text-3xl font-bold">This is the Contact us page</h2>
+      <p class="mt-4 text-gray-600">Let’s build something awesome.</p>
+    </section>
+  </template>
