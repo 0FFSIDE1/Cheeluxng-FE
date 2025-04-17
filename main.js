c6e6d5441b77a7060de96664049cd972fd4e1c7e
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-04-17 18:35:52 +0100

    refactor: restructure main.js for improved readability and maintainability

--- main.js
+++ main.js
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import App from './src/App.vue'
 import router from './src/router'
 import './src/styles/tailwind.css'
+import { createPinia } from 'pinia'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+app.use(createPinia())
+
+app.mount('#app')
