025336e533af659825993a19919550f9dbce86b7
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-04-20 14:27:43 +0100

    feat: add js-cookie dependency for improved cookie management

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
+    port: 5173,
     allowedHosts: ['https://f10a-105-112-203-218.ngrok-free.app'],
     host: true,
   },
