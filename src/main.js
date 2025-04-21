c8a19a1eff112293c64417eb5fe4e4b92ad11d77
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-04-21 22:16:22 +0100

    refactor: standardize code formatting in main.js for consistency

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './styles/tailwind.css'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import './styles/tailwind.css';
+import { createPinia } from 'pinia';
 import csrfPlugin from './plugins/csrf';
 
-const app = createApp(App)
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
+app.use(createPinia());
+app.use(router);
 app.use(csrfPlugin);
-app.mount('#app')
+app.mount('#app');
 
 
   
