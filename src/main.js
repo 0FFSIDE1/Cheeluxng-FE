f23550ba36979f54574a498d6a93a5d11cf6926a
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-04-21 10:41:47 +0100

    refactor: replace initCsrfToken with csrfPlugin for cleaner app initialization

--- src/main.js
+++ src/main.js
@@ -3,14 +3,13 @@ import App from './App.vue'
 import router from './router'
 import './styles/tailwind.css'
 import { createPinia } from 'pinia'
-import { initCsrfToken } from './utils/csrf'
+import csrfPlugin from './plugins/csrf';
 
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
+app.use(csrfPlugin);
+app.mount('#app')
 
-initCsrfToken().then(() => {
-    // Only mount app after CSRF token is set
-    app.mount('#app')
-  })
+
   
