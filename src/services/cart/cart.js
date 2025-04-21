8c31ebd8a5d7804cd789580ff96d2e3cf96da92d
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-04-21 10:42:43 +0100

    refactor: update cart service methods to include CSRF token handling

--- src/services/cart/cart.js
+++ src/services/cart/cart.js
@@ -1,6 +1,19 @@
 import api from "../axios";
+import { fetchCsrfToken } from '../../plugins/csrf';
 
 export const getCart = () => api.get('get-cart');
-export const addToCart = (itemId, data) => api.post(`${itemId}/add-to-cart`, data);
-export const updateCart = (itemId, payload) => api.patch(`${itemId}/update-cart`, payload);
-export const deleteCartItem = (itemId) => api.delete(`${itemId}/delete`);
+
+export const addToCart = async (itemId, data) => {
+  await fetchCsrfToken();
+  return api.post(`${itemId}/add-to-cart`, data);
+};
+
+export const updateCart = async (itemId, payload) => {
+  await fetchCsrfToken();
+  return api.patch(`${itemId}/update-cart`, payload);
+};
+
+export const deleteCartItem = async (itemId) => {
+  await fetchCsrfToken();
+  return api.delete(`${itemId}/delete`);
+};
