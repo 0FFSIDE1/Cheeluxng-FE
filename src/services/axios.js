b460593a5b5bdd9d1992c31d7095b666719d664e
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-04-21 10:43:03 +0100

    refactor: improve CSRF token handling in axios interceptor

--- src/services/axios.js
+++ src/services/axios.js
@@ -11,22 +11,39 @@ const api = axios.create({
   },
 });
 
-// Define safe HTTP methods that require CSRF protection
-const CSRF_PROTECTED_METHODS = ['post', 'put', 'patch', 'delete']
-
 // Interceptor to attach CSRF token to applicable requests
 api.interceptors.request.use(
   (config) => {
-    const method = config.method?.toLowerCase()
-    const csrfToken = Cookies.get('csrftoken')
-
-    if (csrfToken && method && CSRF_PROTECTED_METHODS.includes(method)) {
-      config.headers['X-CSRFToken'] = csrfToken
+    const method = config.method?.toLowerCase();
+    const csrfToken = getCookie('csrftoken');
+    console.log(`About to send ${method} with CSRF:`, Cookies.get('csrftoken'));
+    // Set X-CSRFToken header for unsafe methods
+    if (csrfToken && method && ['post', 'put', 'patch', 'delete'].includes(method)) {
+      config.headers['X-CSRFToken'] = csrfToken;
     }
 
-    return config
+    // Set Referer header to match frontend origin
+    config.headers['x-csrftoken'] = window.location.origin;
+
+    return config;
   },
   (error) => Promise.reject(error)
-)
+);
+
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+      const cookies = document.cookie.split(';');
+      for (let i = 0; i < cookies.length; i++) {
+          const cookie = cookies[i].trim();
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) === (name + '=')) {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
+          }
+      }
+  }
+  return cookieValue;
+}
 
 export default api;
