e7697e9f9d7acb3d93d4e10c883fe6d0e9a5e994
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-04-21 10:41:09 +0100

    refactor: remove CSRF token initialization logic from App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,23 +5,5 @@
 </template>
 
 <script setup>
-import { onMounted } from 'vue'
 import DefaultLayout from './layouts/DefaultLayout.vue'
-import api from './services/axios'
-import Cookies from 'js-cookie'
-
-onMounted(async () => {
-  const token = Cookies.get('csrftoken')
-
-  if (!token) {
-    try {
-      await api.get('/get-csrf-token')
-      console.info('[CSRF] Token retrieved successfully.')
-    } catch (err) {
-      console.error('[CSRF] Failed to fetch token:', err)
-    }
-  } else {
-    console.info('[CSRF] Token already present in cookies.')
-  }
-})
 </script>
