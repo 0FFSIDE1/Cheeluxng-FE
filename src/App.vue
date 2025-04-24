c226dcd308896d7377f1f3ed9aa666d622734872
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-04-24 23:46:06 +0100

    added preloader

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
-  <DefaultLayout>
-    <router-view />
-  </DefaultLayout>
+  <div>
+    <Preloader v-if="loading"/>
+    <DefaultLayout v-else>
+      <router-view />
+    </DefaultLayout>
+  </div>
 </template>
 
 <script setup>
 import DefaultLayout from './layouts/DefaultLayout.vue'
+import Preloader from './components/Preloader.vue';
+import { ref, onMounted} from 'vue';
+
+const loading = ref(true);
+
+onMounted(() => {
+  setTimeout(() => {
+    loading.value = false; 
+  }, 3100);
+});
 </script>
