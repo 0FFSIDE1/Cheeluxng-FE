ef2777dee65d3cf9cafcf68fc44f253dcad85394
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-04-25 07:50:49 +0100

    changes

--- src/main.js
+++ src/main.js
@@ -4,8 +4,17 @@ import router from './router';
 import './styles/tailwind.css';
 import { createPinia } from 'pinia';
 import csrfPlugin from './plugins/csrf';
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
 
 const app = createApp(App);
+app.use(
+    Toast, {
+        transition: 'Vue-Toastification__bounce',
+        maxToasts: 5,
+        newestOnTop: true,
+    }
+);
 app.use(createPinia());
 app.use(router);
 app.use(csrfPlugin);
