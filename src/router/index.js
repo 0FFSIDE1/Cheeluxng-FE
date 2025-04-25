f9e333b678cabcf69cca7bb83ebebd9058bd48c3
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-04-25 20:42:36 +0100

    changes

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   { path: '/', component: () => import('../pages/Home.vue') },
-  { path: '/:category', name:'ProductsByCategory', component: () => import('../pages/Products.vue'), props: route => ({ category: route.params.category }) },
+  { path: '/:category', name:'ProductsByCategory', component: () => import('../pages/ProductsByCategory.vue'), props: route => ({ category: route.params.category }) },
   { path: '/explore', component: () => import('../pages/Explore.vue') },
+  { path: '/product/:id', component: () => import('../pages/Explore.vue') },
+
 ]
 
 const router = createRouter({
