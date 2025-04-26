3896bcfeca96f816856b586695af018a52fc4fa9
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-04-26 02:28:40 +0100

    changes

--- src/services/product/Products.js
+++ src/services/product/Products.js
@@ -1,3 +1,6 @@
 import api from "../axios";
 
 export const fetchAllProducts = (params = {}) => api.get('products', { params }); 
+export const fetchAllProductsById = async (id) => {
+    return api.get(`product/${id}`);
+};
