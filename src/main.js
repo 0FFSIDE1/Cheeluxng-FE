2771df1e77fc0d7246ee6b495a17520de2ca95f9
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-04-26 02:28:17 +0100

    changes

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import { createPinia } from 'pinia';
 import csrfPlugin from './plugins/csrf';
 import Toast from 'vue-toastification';
 import 'vue-toastification/dist/index.css';
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
 
 const app = createApp(App);
 app.use(
@@ -15,6 +18,7 @@ app.use(
         newestOnTop: true,
     }
 );
+AOS.init()
 app.use(createPinia());
 app.use(router);
 app.use(csrfPlugin);
