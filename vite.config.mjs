61416445404ae2867c62e7f1d29a44e928df8a2b
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-04-26 20:29:52 +0100

    changes

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,34 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import viteCompression from 'vite-plugin-compression'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteCompression({
+      algorithm: 'gzip',
+      ext: '.gz',
+      threshold: 10240,
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false, // optional for production
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['vue'],
+        }
+      }
+    }
+  },
+  base: '/',
   server: {
     port: 5173,
     allowedHosts: [],
