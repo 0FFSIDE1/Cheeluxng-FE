ff0e37c7815f25ab42d434feb8913d1861f3fdfd
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-04-26 12:50:32 +0100

    4 sec prelloader and scroll to top btn

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <Preloader v-if="loading"/>
     <DefaultLayout v-else>
       <router-view />
+      <ScrollToTop />
     </DefaultLayout>
   </div>
 </template>
@@ -11,12 +12,13 @@
 import DefaultLayout from './layouts/DefaultLayout.vue'
 import Preloader from './components/Preloader.vue';
 import { ref, onMounted} from 'vue';
+import ScrollToTop from './components/ScrollToTopBtn.vue';
 
 const loading = ref(true);
 
 onMounted(() => {
   setTimeout(() => {
     loading.value = false; 
-  }, 3100);
+  }, 4000);
 });
 </script>
