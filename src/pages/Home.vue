6afbc2d7cb25ce58d093491bccfe002ee820197c
OFFSIDE <somtoemmanuel6@gmail.com>
2025-04-29 13:14:49 +0100

    Update Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -59,7 +59,7 @@
     <div v-else class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
       <div class="flex flex-col gap-4">
         <!-- Centered Title -->
-        <div class="w-full flex justify-center">
+        <div class="w-full">
           <h2
             class="prose prose-lg text-gray-900 text-lg sm:text-xl md:text-2xl lg:text-3xl font-semibold leading-snug animate-fade-in"
           >
