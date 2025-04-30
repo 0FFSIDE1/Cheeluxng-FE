a6f519f0595ddeb8229fc1633dcc95a5c845be2f
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-04-30 17:02:34 +0100

    fixs

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,15 +12,15 @@ export default {
         lato: ['Lato', 'sans-serif'],
       },
     },
-    // screens: {
-    //   'xs': '400px',         // custom breakpoint
-    //   'sm': '896px',
-    //   'md': '768px',
-    //   'lg': '1024px',
-    //   'xl': '1280px',
-    //   '2xl': '1536px',
-    //   '3xl': '1920px'        // another custom one
-    // },
+    screens: {
+      'xs': '400px',         // custom breakpoint
+      'sm': '896px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      '3xl': '1920px'        // another custom one
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
