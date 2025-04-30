bfabf8ff7f8cefa86b7fa5807d7d8822ca9e1d3a
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-04-30 16:31:21 +0100

    fixs

--- src/plugins/csrf.js
+++ src/plugins/csrf.js
@@ -5,7 +5,6 @@ export const fetchCsrfToken = async () => {
   if (!Cookies.get('csrftoken')) {
     try {
       const response = await api.get('get-csrf-token'); // ensure this endpoint matches your Django route
-      console.log('CSRF token fetched:', Cookies.get('csrftoken'), response.data.csrfToken);
       return response.data.csrfToken;
     } catch (error) {
       console.error('Error fetching CSRF token:', error);
