08bf12c4f7ebd8aa7bbdf5aece64425cf191a471
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-05-01 16:24:32 +0100

    fixs

--- src/pages/SingleProduct.vue
+++ src/pages/SingleProduct.vue
@@ -13,7 +13,7 @@
     <div v-else class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 mb-6">
     <button
     @click="goBack"
-    class="inline-flex items-center my-4 px-4 py-2 bg-white text-gray-900 rounded-lg shadow hover:bg-gray-500 hover:text-white transition-colors focus:outline-none focus:ring-2 focus:ring-primary"
+    class="inline-flex items-center px-4 py-2 bg-white text-gray-900 rounded-lg shadow hover:bg-gray-900 hover:text-white transition-colors focus:outline-none focus:ring-2 focus:ring-primary"
      >
     ← Back
     </button>
